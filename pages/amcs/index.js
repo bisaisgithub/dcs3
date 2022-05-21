5dfa81fdd1170b001c0af2d8934b2d2d8d49c917
unknown <benaremail@gmail.com>
2022-05-21 21:16:58 +0800

    m

---
+++ pages/amcs/index.js
@@ -0,0 +1,15 @@
+import AMCSNavbarPatient from "../../components/amcs/AMCSNavbarPatient";
+
+
+const AMCSHome = () => {
+  return (
+    <div>
+      <AMCSNavbarPatient/>
+      <div className="hero" id="#home">
+
+      </div>
+    </div>
+  );
+};
+
+export default AMCSHome;
---
+++ pages/amcs/login/index.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from 'next/link';
+import AMCSNavbarPatient from "../../../components/amcs/AMCSNavbarPatient";
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const login = async (e) => {
+    e.preventDefault();
+    alert('Function is not yet done')
+    // const credentials = { email, password };
+    // const user = await axios.post(
+    //   "/api/cdcs/login",
+    //   credentials
+    // );
+    // console.log(user);
+    // if (user.data.success) {
+    //   console.log("router push should run after");
+    //   router.push("/cdcs/dashboard");
+    // }else{
+    //   alert('Invalid Email or Password');
+    // }
+  };
+  return (
+    <div className='details-details-container'>
+      <AMCSNavbarPatient/>
+      <form className="form-container-login" onSubmit={login}>
+        <div className="form-body-login">
+        <div className="form-title-container">
+          <div className='form-title-text'>
+            Login
+          </div>
+        </div>
+          <div className="form-body-input-box">
+            <span className="form-body-input-box-span">Email</span>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </div>
+          <div className="form-body-input-box">
+            <span className="form-body-input-box-span">Password</span>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <p className="forgot_password">Forgot Password? Click <Link href={`/cdcs/forgotpassword`}>Here</Link></p>
+          <div className="details-details-modal-body-button">
+            <button type="submit">Login</button>
+          </div>
+          <p>You do not have an account? Register <Link href={`/cdcs/register`}>Here</Link></p>
+        </div>
+        
+      </form>
+    </div>
+  );
+};
+
+export default Login;
