bd462043b04cac7c433fb140d8f17cf7ead8bb24
Benar Isais <benaremail@gmail.com>
2022-06-11 17:09:51 +0800

    m

--- components/cdcs/Inventory/add.js
+++ components/cdcs/Inventory/add.js
@@ -2,7 +2,6 @@
 let inventoryAdd = [
   {
     _id: 1,
-    add_id: '',
     supplier: 's1',
     invoice: 'inv1',
     date_received: 'd1',
@@ -11,37 +10,25 @@ let inventoryAdd = [
       {
         description: 'd1',
         quantity: 10,
-        unit_price: 100
+        unit_price: 100,
+        used: 0
       },
       {
         description: 'd2',
         quantity: 10,
-        unit_price: 200
+        unit_price: 200,
+        used: 0
       },
     ]
   }
 ]
 
+// let inventories: [{name: String, code: String, qty: Number}],
+
 let inventoryDeduct = [
   {
-    _id: 1,
-    add_id: 'ObjectId',
-    appointment_id: 'ObjectId',
-    // supplier: 's1',
-    // invoice: 'inv1',
-    // date_received: 'd1',
-    // type: 'add',
-    items:[
-      {
-        description: 'd1',
-        quantity: 10,
-        unit_price: 100
-      },
-      {
-        description: 'd2',
-        quantity: 10,
-        unit_price: 200
-      },
-    ]
+    name: 'd1',
+    code: 'id1',
+    qty: 5
   }
 ]
